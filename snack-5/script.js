8d1c4b04096b8df47bf0b45511766385b0fee470
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-11-05 16:00:48 +0100

    05

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,12 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const pari = [];
+
+for (let i = 0 ; i < nums.length; i++){
+    if(nums[i] % 2 === 0){
+        pari.push(nums[i]);
+    };
+};
+console.log(pari);
